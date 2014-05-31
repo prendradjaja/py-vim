dda3aac56261ce27762c0e2eeeb2297d3e15a61b
Pandu <prendradjaja@gmail.com>
2014-05-31 11:31:27 -0700

    Use unlines() to make test code prettier

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import vim
+import util
 
 class BufferTests(unittest.TestCase):
     def setUp(self):
@@ -8,22 +9,22 @@ class BufferTests(unittest.TestCase):
     def testReplace(self):
         self.buffer.replace_lines(2, 3, ("foo", "bar"))
         self.assertEqual(self.buffer.dump(),
-                "lorem\nfoo\nbar\nsit\n")
+                util.unlines(["lorem", "foo", "bar", "sit"]))
 
     def testReplaceHead(self):
         self.buffer.replace_lines(1, 1, ("foo", "bar"))
         self.assertEqual(self.buffer.dump(),
-                "foo\nbar\nipsum\ndolor\nsit\n")
+                util.unlines(["foo", "bar", "ipsum", "dolor", "sit"]))
 
     def testReplaceTail(self):
         self.buffer.replace_lines(4, 4, ("foo", "bar"))
         self.assertEqual(self.buffer.dump(),
-                "lorem\nipsum\ndolor\nfoo\nbar\n")
+                util.unlines(["lorem", "ipsum", "dolor", "foo", "bar"]))
 
     def testReplaceWithNothing(self):
         self.buffer.replace_lines(2, 3, ())
         self.assertEqual(self.buffer.dump(),
-                "lorem\nsit\n")
+                util.unlines(["lorem", "sit"]))
 
 if __name__ == '__main__':
     unittest.main()
--- util.py
+++ util.py
@@ -3,3 +3,8 @@
 def split_at(index, seq):
     """Split a sequence into two at a given index and return the two parts"""
     return seq[:index], seq[index:]
+
+def unlines(lines):
+    """Turn a sequence of strings into a single string, with newlines after
+    each."""
+    return '\n'.join(lines) + '\n'
